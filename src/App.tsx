7fee2b32d4a2f84dfa66fff7357c80599c4b3582
mattwilkson <60830060+mattwilkson@users.noreply.github.com>
2025-06-29 20:57:13 -0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-// File: src/App.tsx
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { LandingPage } from './pages/LandingPage'
-import { AuthPage } from './pages/AuthPage'
-import { OnboardingPage } from './pages/OnboardingPage'
-import { DashboardPage } from './pages/DashboardPage'
+import AuthPage from './pages/AuthPage'
+import OnboardingPage from './pages/OnboardingPage'
+import DashboardPage from './pages/DashboardPage'
 
 import { AuthProvider } from './hooks/useAuth'
-import { ProtectedRoute } from './components/ProtectedRoute'
+import ProtectedRoute from './components/ProtectedRoute'
 
 export function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          {/* Public landing & auth */}
           <Route path="/" element={<LandingPage />} />
           <Route path="/auth" element={<AuthPage />} />
-
-          {/* Onboarding: allowWithoutFamily shows this for brand-new users */}
           <Route
             path="/onboarding"
             element={
-              <ProtectedRoute allowWithoutFamily={true}>
+              <ProtectedRoute allowWithoutFamily>
                 <OnboardingPage />
               </ProtectedRoute>
             }
           />
-
-          {/* All other routes require a family */}
           <Route
             path="/dashboard/*"
             element={
